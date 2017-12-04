ee7c2531083ed4cf0d8dc3df5f1e20187cb974aa
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2017-12-04 11:28:27 +0530

    Update Carts.java

--- src/main/java/com/absyz/service/Carts.java
+++ src/main/java/com/absyz/service/Carts.java
@@ -107,8 +107,8 @@ public class Carts {
 		try {
 			//String strQuery = "Select * from carts where userid = "+intUserId; 
 				
-			String strQuery = "Select c.cartid,c.userid,c.productid,c.quantity,c.amount,p.productname,p.price from carts c where c.userid ="+intUserId;
-					//+ "join products p on c.productid = p.productid where c.userid ="+intUserId;
+			String strQuery = Select c.cartid,c.userid,c.productid,c.quantity,c.amount,p.productname,p.price from carts c "
+					+ "join products p on c.productid = p.productid where c.userid = "+intUserId;
 				
 			//console.log(intUserId);
 			conn = DbConnection.getConnection();
