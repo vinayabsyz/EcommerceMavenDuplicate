45bce0d63807b69736485e97c66debdc08a4789f
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2017-12-25 10:07:08 +0530

    Update EcommerceServlet.java

--- src/main/java/com/absyz/core/EcommerceServlet.java
+++ src/main/java/com/absyz/core/EcommerceServlet.java
@@ -48,6 +48,7 @@ response.setContentType("application/json");
 			    u.doPost(request,response);
 			      // Set response content type
 			      response.setContentType("text/html");
+			   out.println("<h1></h1>");
 //response.setContentType("application/json");
 			      // Actual logic goes here.
 			     
