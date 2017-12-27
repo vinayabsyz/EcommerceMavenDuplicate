c8960a503587fd13c8239bd2f2b64da3029809bc
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2017-12-27 12:19:27 +0530

    Update uploadservlet1.java

--- src/main/java/com/absyz/core/uploadservlet1.java
+++ src/main/java/com/absyz/core/uploadservlet1.java
@@ -33,8 +33,12 @@ public class uploadservlet1 extends HttpServlet {
 	   public void init( ){
 	      // Get the file location where it would be stored.
 	       //filePath = getServletContext().getInitParameter("file-path"); 
-		uploadservlet1 u=new uploadservlet1();
+		try {
+		   uploadservlet1 u=new uploadservlet1();
 		   filePath = u.getPath(); 
+		}
+		   catch (UnsupportedEncodingException e) {
+		   }
 	   }
 	   public void doPost(HttpServletRequest request,    HttpServletResponse response)   throws ServletException, java.io.IOException {
 		   
