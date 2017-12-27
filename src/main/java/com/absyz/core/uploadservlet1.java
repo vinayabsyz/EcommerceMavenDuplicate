d9a63d845081a78dea574c0f59630da840062004
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2017-12-27 12:03:00 +0530

    Update uploadservlet1.java

--- src/main/java/com/absyz/core/uploadservlet1.java
+++ src/main/java/com/absyz/core/uploadservlet1.java
@@ -14,7 +14,7 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import java.net.URLEncoder;
+import java.net.URLDecoder;
 import javax.servlet.http.Part;
 /**
  * Servlet implementation class UploadServlet
