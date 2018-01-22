2969cbdd2213a894922ec588b07a1e596924e7f0
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-01-22 13:23:58 +0530

    Update Jscript.js

--- WebContent/js/Jscript.js
+++ WebContent/js/Jscript.js
@@ -279,7 +279,20 @@ $(document).on("click","#btnBack",function(){
 })
 //Button to change password
 $(document).on("click","#btnChngPwd",function(){
-	
+	<table class="login-screen">
+				<tr class='tbl_header'>
+					<td colspan='2'> <h2>Change Password</h2> </td>
+				</tr>
+				<tr>
+					<td>Password</td><td><input type="text" name="txtPwd" id="txtPwd" /></td>
+				</tr>
+				<tr>
+					<td>Confirm Password</td><td><input type="password" name="txtCfrmPwd" id="txtCfrmPwd" /></td>
+				</tr>
+				<tr>
+					<td><input type="button" id="btnSave" value="Save" /><input type="button" id="btnUser" value="Back" /></td>
+				</tr>
+			</table>
 	alert('vv');
 	$('#divcontent').hide();
 	$('#userdiv').hide();
