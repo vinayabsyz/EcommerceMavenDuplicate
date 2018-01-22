205708cfcdfa2b9b7f9f6f8cd9bd21056bd9d88b
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-01-22 16:22:11 +0530

    Update Jscript.js

--- WebContent/js/Jscript.js
+++ WebContent/js/Jscript.js
@@ -584,9 +584,11 @@ function showuserinfo()
 				usertable = usertable + "<tr class='tile'><td class='tbl_header'><strong>City</strong></td><td>"+obj[0].data[0].city+"</td><td class='tbl_header'><strong>State</strong></td><td>"+obj[0].data[0].state+"</td></tr>";
 				usertable = usertable + "<tr class='tile'><td class='tbl_header'><strong>Country</strong></td><td>"+obj[0].data[0].country+"</td><td class='tbl_header'><strong>Zipcode</strong></td><td>"+obj[0].data[0].zipcode+"</td></tr></table>";
 								
-				//usertable = usertable + "<tr><td colspan='4' align='center'><input type='button' id='btnChngPwd' value='Change Password' /></td></tr></table>";
+				usertable = usertable + "<tr><td colspan='4' align='center'><input type='button' id='btnChngPwd' value='Change Password' /></td></tr></table>";
 				//usertable = usertable + "<tr><td colspan='4' align='center'><img  alt='product' src='images/download.jpg' width='250px' height='250px'/></td></tr>
-			
+			 $("#btnChngPwd").click(function(){
+        alert("button");
+    }); 
 				$('#menu5').empty();
 				
 				$('#menu5').append(usertable);
