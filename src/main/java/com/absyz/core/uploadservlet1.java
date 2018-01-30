040fd8fe27760a87f6674792ff90f9659e2bc38c
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-01-30 15:34:11 +0530

    Update uploadservlet1.java

--- src/main/java/com/absyz/core/uploadservlet1.java
+++ src/main/java/com/absyz/core/uploadservlet1.java
@@ -1,138 +1,78 @@
-//package codeTpoint;
-package main.java.com.absyz.core;
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
+import java.io.InputStream;
 import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import java.io.UnsupportedEncodingException;
-
-import java.net.URLDecoder;
-import javax.servlet.http.Part;
-/**
- * Servlet implementation class UploadServlet
- */
-public class uploadservlet1 extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-       
-	private boolean isMultipart;
-	   private String filePath;
-	   private int maxFileSize = 50 * 1024;
-	   private int maxMemSize = 4 * 1024;
-	   private File file ;
-
-	   public void init( ){
-	      // Get the file location where it would be stored.
-	       //filePath = getServletContext().getInitParameter("file-path"); 
-		
-		   //uploadservlet1 u=new uploadservlet1();
-		   filePath ="\\WebContent\\images"; 
-		
-		  
-		   
-	   }
-	   public void doPost(HttpServletRequest request,    HttpServletResponse response)   throws ServletException, java.io.IOException {
-		   
-		   // Check that we have a file upload request
-	      isMultipart = ServletFileUpload.isMultipartContent(request);
-	      response.setContentType("text/html");
-	      java.io.PrintWriter out = response.getWriter( );
-
-	      
-	      
-	      if( !isMultipart )
-	      {
-	         out.println("No Upload This Time");
-	         return;
-	      }
-	      DiskFileItemFactory factory = new DiskFileItemFactory();
-	      
-	      
-	      // maximum size that will be stored in memory
-	      factory.setSizeThreshold(maxMemSize);
-     
-	      // Location to save data that is larger than maxMemSize.
-	      factory.setRepository(new File("WebContent"));
-
-	      // Create a new file upload handler
-	      ServletFileUpload up = new ServletFileUpload(factory);
-	      
-	      
-
-	      up.setSizeMax( maxFileSize );
-
-	      try{ 
-
-	      List fileItems = up.parseRequest(request);
-		
-
-	      Iterator element = fileItems.iterator();
- 
-	      out.println("<html>");
-	      out.println("<head>");
-	      out.println("<title>Servlet upload</title>");  
-	      out.println("</head>");
-	      out.println("<body>");
-	      while ( element.hasNext () ) 
-	      {
-	        FileItem fi = (FileItem)element.next();
-	         if ( !fi.isFormField () )	
-	         {
-	            
-	            String fieldName = fi.getFieldName();
-	            String fileName = fi.getName();
-	            String contentType = fi.getContentType();
-	            boolean isInMemory = fi.isInMemory();
-	            long sizeInBytes = fi.getSize();
-	            				 
-	            
-	            if( fileName.lastIndexOf("\\") >= 0 ){
-	               file = new File( filePath +"\\"+ fileName.substring( fileName.lastIndexOf("\\"))) ;
-	            }else{
-	               file = new File( filePath +"\\"+fileName.substring(fileName.lastIndexOf("\\")+1)) ;
-	            }
-	            
-	          
-	            fi.write( file ) ;
-	            out.println("Uploaded Filename: " + fileName + "<br>"+ filePath);
-	         }
-	      }
-	      out.println("</body>");
-	      out.println("</html>");
-	   }catch(Exception ex) {
-	       System.out.println(ex);
-	   } 
-	   }
-
-	   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException { doPost(request, response); }
-	   	    
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+public class AmazonS3Example {
 	
-public String getPath() throws UnsupportedEncodingException {
-	try {
-String path = this.getClass().getClassLoader().getResource("").getPath();
-String fullPath = URLDecoder.decode(path, "UTF-8");
-String pathArr[] = fullPath.split("/WEB-INF/classes/");
-System.out.println(fullPath);
-System.out.println(pathArr[0]);
-fullPath = pathArr[0];
-String reponsePath = "";
-// to read a file from webcontent
-reponsePath = new File(fullPath).getPath() + File.separatorChar;
-return reponsePath;
-}
-
-	catch (UnsupportedEncodingException e) {
-	e.printStackTrace();
-		return null;
+	private static final String SUFFIX = "/";
+	
+	public static void main(String[] args) {
+		// credentials object identifying user for authentication
+		// user must have AWSConnector and AmazonS3FullAccess for 
+		// this example to work
+		AWSCredentials credentials = new BasicAWSCredentials(
+				"AKIAIQT7Y5EDYZQFWTBA", 
+				"kd7Qb/ML/NVubxjVd+EGZJlHincFI5AGnLTjVW+P");
+		
+		// create a client connection based on credentials
+		AmazonS3 s3client = new AmazonS3Client(credentials);
+		
+		// create bucket - name must be unique for all S3 users
+		String bucketName = "javatutorial-net-example-bucket";
+		s3client.createBucket(bucketName);
+		
+		// list buckets
+		for (Bucket bucket : s3client.listBuckets()) {
+			System.out.println(" - " + bucket.getName());
+		}
+		
+		// create folder into bucket
+		String folderName = "testfolder";
+		createFolder(bucketName, folderName, s3client);
+		
+		// upload file to folder and set it to public
+		String fileName = folderName + SUFFIX + "testvideo.mp4";
+		s3client.putObject(new PutObjectRequest(bucketName, fileName, 
+				new File("C:\\Users\\vishwasamudralogo.PNG"))
+				.withCannedAcl(CannedAccessControlList.PublicRead));
+		
+		deleteFolder(bucketName, folderName, s3client);
+		
+		// deletes bucket
+		s3client.deleteBucket(bucketName);
 	}
-}
+	
+	public static void createFolder(String bucketName, String folderName, AmazonS3 client) {
+		// create meta-data for your folder and set content-length to 0
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentLength(0);
+		// create empty content
+		InputStream emptyContent = new ByteArrayInputStream(new byte[0]);
+		// create a PutObjectRequest passing the folder name suffixed by /
+		PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName,
+				folderName + SUFFIX, emptyContent, metadata);
+		// send request to S3 to create folder
+		client.putObject(putObjectRequest);
+	}
+	/**
+	 * This method first deletes all the files in given folder and than the
+	 * folder itself
+	 */
+	public static void deleteFolder(String bucketName, String folderName, AmazonS3 client) {
+		List fileList = 
+				client.listObjects(bucketName, folderName).getObjectSummaries();
+		for (S3ObjectSummary file : fileList) {
+			client.deleteObject(bucketName, file.getKey());
+		}
+		client.deleteObject(bucketName, folderName);
+	}
 }
