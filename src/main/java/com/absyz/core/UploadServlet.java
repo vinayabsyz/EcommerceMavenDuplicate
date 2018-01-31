f7d9ad7d30cfdb70c27706020fff57f432b2ff54
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-01-31 07:40:18 +0530

    Update UploadServlet.java

--- src/main/java/com/absyz/core/UploadServlet.java
+++ src/main/java/com/absyz/core/UploadServlet.java
@@ -2,7 +2,9 @@ package main.java.com.absyz.core;
 
 import java.io.*;
 import java.util.*;
- 
+ import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +16,16 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.output.*;
-
+import java.util.List;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import main.java.com.absyz.service.Products;
 
 public class UploadServlet extends HttpServlet {
@@ -30,6 +41,33 @@ public class UploadServlet extends HttpServlet {
       //filePath = getServletContext().getInitParameter("file-upload"); 
 	  // filePath = "C:\\Files\\";
 	  filePath = "WebContent/images/";
+	   AWSCredentials credentials = new BasicAWSCredentials(
+				"AKIAIQT7Y5EDYZQFWTBA", 
+				"kd7Qb/ML/NVubxjVd+EGZJlHincFI5AGnLTjVW+P");
+		
+		// create a client connection based on credentials
+		AmazonS3 s3client = new AmazonS3Client(credentials);
+		
+		// create bucket - name must be unique for all S3 users
+		String bucketName = "javatutorial-net-example-bucket";
+		s3client.createBucket(bucketName);
+		
+		// list buckets
+		for (Bucket bucket : s3client.listBuckets()) {
+			System.out.println(" - " + bucket.getName());
+		}
+		
+		// create folder into bucket
+		String folderName = "testfolder";
+		createFolder(bucketName, folderName, s3client);
+		
+		// upload file to folder and set it to public
+		String fileName = folderName + SUFFIX + "testvideo.mp4";
+		s3client.putObject(new PutObjectRequest(bucketName, fileName, 
+				new File("C:\\Users\\vishwasamudralogo.PNG"))
+				.withCannedAcl(CannedAccessControlList.PublicRead));
+		
+		
    }
    
    public void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -145,5 +183,17 @@ public class UploadServlet extends HttpServlet {
          throw new ServletException("GET method used with " +
             getClass( ).getName( )+": POST method required.");
       }
+	public static void createFolder(String bucketName, String folderName, AmazonS3 client) {
+		// create meta-data for your folder and set content-length to 0
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentLength(0);
+		// create empty content
+		InputStream emptyContent = new ByteArrayInputStream(new byte[0]);
+		// create a PutObjectRequest passing the folder name suffixed by /
+		PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName,
+				folderName + SUFFIX, emptyContent, metadata);
+		// send request to S3 to create folder
+		client.putObject(putObjectRequest);
+	}
    }
 
