10818f481ede95b28a443ea0942d36cd711a2d17
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-01-31 10:06:41 +0530

    Update UploadServlet.java

--- src/main/java/com/absyz/core/UploadServlet.java
+++ src/main/java/com/absyz/core/UploadServlet.java
@@ -64,7 +64,7 @@ private static final String SUFFIX = "/";
 		// upload file to folder and set it to public
 		String fileName = folderName + SUFFIX + "testvideo.mp4";
 		s3client.putObject(new PutObjectRequest(bucketName, fileName, 
-				new File("WebContent\\images\\dell_laptop.jpg"))
+				new File("WebContent//images//dell_laptop.jpg"))
 				.withCannedAcl(CannedAccessControlList.PublicRead));
 		
 		
