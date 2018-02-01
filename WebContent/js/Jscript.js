28e4341a23580d0e421dd3e97ea400846cfd14e8
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-02-01 13:40:31 +0530

    Update Jscript.js

--- WebContent/js/Jscript.js
+++ WebContent/js/Jscript.js
@@ -757,7 +757,7 @@ function add_totalamount(id)
 			//prod_amount = $(this).find("td").eq(3).html()parseInt($('#amt_'+rowCount).html())*parseInt($('#qty_'+rowCount).val());	
 			var amt =  $(MyRows[id]).find('td:eq(3)').html();
 			var qty =  $('#qty_'+cartarray[id-2]).val();
-			if(qty.length=0){
+			if(qty=""){
 			qty=0;
 			}
 			prod_amount = $(MyRows[id]).find('td:eq(3)').html()*parseInt(qty);	
