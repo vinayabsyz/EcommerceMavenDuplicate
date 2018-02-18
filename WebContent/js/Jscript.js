9e6beff3aaf264bc2c2f8c0a4000367745f91775
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-02-18 08:41:23 +0530

    Update Jscript.js

--- WebContent/js/Jscript.js
+++ WebContent/js/Jscript.js
@@ -431,7 +431,7 @@ function hidechangepwdCr(){
 }
 function loadProducts()
 {
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
 	$('#divcontent').show();
 	$('#divhome').hide();
 	$('#product_list').show();
