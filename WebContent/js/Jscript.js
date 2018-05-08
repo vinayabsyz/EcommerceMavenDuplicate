2e6360d723464fc124cda76911f2e822b71759bd
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-05-08 13:15:09 +0530

    Update Jscript.js

--- WebContent/js/Jscript.js
+++ WebContent/js/Jscript.js
@@ -226,7 +226,7 @@ $(document).on("click", "#td_myorders", function() {
 			alert(obj[0].data.length);
 			//alert(obj[0].data[0].ProductName__c);
 			
-			var orderTable="<br/><br/><b>Pending Orders</b><table width='100%'><br/><table width='100%'><tr class='tile'><td><strong>Product Name</strong></td><td><strong>Date</strong></td><td><strong>Amount</strong></td><td><strong>Quantity</strong></td><td><strong>Additional Charges</strong></td><td><strong>Amount</strong></td><td><strong>status</strong></td>";
+			var orderTable="<br/><br/><b>Pending Orders</b><table width='100%'><br/><table width='100%'><tr class='tile'><td><strong>Product Name</strong></td><td><strong>Date</strong></td><td><strong>price</strong></td><td><strong>Quantity</strong></td><td><strong>Additional Charges</strong></td><td><strong>Amount</strong></td><td><strong>status</strong></td>";
 			for(var i=0;i<obj[0].data.length;i++)
 			{
 				alert("ProductName"+obj[0].data[i].ProductName__c);
@@ -235,12 +235,12 @@ $(document).on("click", "#td_myorders", function() {
 				if(i%2 == 0)
 				{
 					orderTable = orderTable + "<tr class='tile'><td>"+obj[0].data[i].ProductName__c+"</td>" +
-					"<td>"+obj[0].data[i].Order_Date__c+"</td><td>"+obj[0].data[i].price__c	+"</td><td>"+obj[0].data[i].productquantity__c+"</td><td>100</td><td>"+totamt+"</td><td>"+obj[0].data[i].status__c+"</td></tr>";
+					"<td>"+obj[0].data[i].order_date__c+"</td><td>"+obj[0].data[i].price__c	+"</td><td>"+obj[0].data[i].productquantity__c+"</td><td>100</td><td>"+totamt+"</td><td>"+obj[0].data[i].status__c+"</td></tr>";
 					}
 				else
 				{
 					orderTable = orderTable + "<tr class='tile'><td>"+obj[0].data[i].ProductName__c+"</td>" +
-					"<td>"+obj[0].data[i].Order_Date__c+"</td><td>"+obj[0].data[i].price__c	+"</td><td>"+obj[0].data[i].productquantity__c+"</td><td>100</td><td>"+totamt+"</td><td>"+obj[0].data[i].status__c+"</td></tr>";
+					"<td>"+obj[0].data[i].order_date__c+"</td><td>"+obj[0].data[i].price__c	+"</td><td>"+obj[0].data[i].productquantity__c+"</td><td>100</td><td>"+totamt+"</td><td>"+obj[0].data[i].status__c+"</td></tr>";
 					}
 				}
 				}
@@ -295,12 +295,12 @@ $(document).on("click", "#td_myorders", function() {
 				if(obj[0].data[i].status!="Order Delivered"){
 				if(i%2 == 0)
 				{
-					orderTable = orderTable + "<tr class='tile'><td>"+obj[0].data[i].Product_Name__c+"</td>" +
+					orderTable = orderTable + "<tr class='tile'><td>"+obj[0].data[i].productname__c+"</td>" +
 					"<td>"+obj[0].data[i].Order_Date__c+"</td><td>"+obj[0].data[i].Product_Price__c+"</td><td>"+obj[0].data[i].productquantity__c+"</td><td>100</td><td>"+totamt+"</td><td><button type='button' class='button' id="+obj[0].data[i].orderid+" onclick='changestatus(this.id)'>changestatus</button></td></tr>";
 					}
 				else
 				{
-					orderTable = orderTable + "<tr class='tile'><td>"+obj[0].data[i].Product_Name__c+"</td>" +
+					orderTable = orderTable + "<tr class='tile'><td>"+obj[0].data[i].productname__c+"</td>" +
 					"<td>"+obj[0].data[i].Order_Date__c+"</td><td>"+obj[0].data[i].Product_Price__c+"</td><td>"+obj[0].data[i].productquantity__c+"</td><td>100</td><td>"+totamt+"</td><td><button type='button' class='button' id="+obj[0].data[i].orderid+" onclick='changestatus(this.id)'>changestatus</button></td></tr>";
 					}
 				}
@@ -317,12 +317,12 @@ $(document).on("click", "#td_myorders", function() {
 				if(obj[0].data[i].status=="Order Delivered"){
 				if(i%2 == 0)
 				{
-					orderTable1 = orderTable1 + "<tr class='tile'><td>"+obj[0].data[i].Product_Name__c+"</td>" +
+					orderTable1 = orderTable1 + "<tr class='tile'><td>"+obj[0].data[i].productname__c+"</td>" +
 					"<td>"+obj[0].data[i].Order_Date__c+"</td><td>"+obj[0].data[i].Product_Price__c+"</td><td>"+obj[0].data[i].productquantity__c+"</td><td>100</td><td>"+totamt+"</td><td>"+obj[0].data[i].status__c+"</td></tr>";
 					}
 				else
 				{
-					orderTable1 = orderTable1 + "<tr class='tile'><td>"+obj[0].data[i].Product_Name__c+"</td>" +
+					orderTable1 = orderTable1 + "<tr class='tile'><td>"+obj[0].data[i].productname__c+"</td>" +
 					"<td>"+obj[0].data[i].Order_Date__c+"</td><td>"+obj[0].data[i].Product_Price__c+"</td><td>"+obj[0].data[i].productquantity__c+"</td><td>100</td><td>"+totamt+"</td><td>"+obj[0].data[i].status__c+"</td></tr>";
 					}
 				}
